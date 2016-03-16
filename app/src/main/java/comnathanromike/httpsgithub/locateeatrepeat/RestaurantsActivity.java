0a738b5b6e7e8124996398ab4cd6b1b1e0562f99
Nathan Romike <nathanromike@gmail.com>
2016-03-15 17:38:25 -0700

    Add array adapter to restaurants activity.

--- app/src/main/java/comnathanromike/httpsgithub/locateeatrepeat/RestaurantsActivity.java
+++ app/src/main/java/comnathanromike/httpsgithub/locateeatrepeat/RestaurantsActivity.java
@@ -3,9 +3,14 @@ package comnathanromike.httpsgithub.locateeatrepeat;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class RestaurantsActivity extends AppCompatActivity {
+    private ListView mListView;
+    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro", "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",  "Slappy Cakes", "Equinox", "Miss Delta's", "Andina", "Lardo", "Portland City Grill", "Fat Head's Brewery", "Chipotle", "Subway"};
+
     public static final String TAG = RestaurantsActivity.class.getSimpleName();
     private TextView mLocationTextView;
 
@@ -13,6 +18,10 @@ public class RestaurantsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+
+        mListView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
+        mListView.setAdapter(adapter);
 
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
 
