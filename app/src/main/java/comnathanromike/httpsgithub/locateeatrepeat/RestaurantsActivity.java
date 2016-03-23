0ef8a1cf75f3dc5dfed9ce9948803fae4569d0d4
NathanRomike <nathanromike@gmail.com>
2016-03-22 19:53:55 -0700

    Fix getRestaurants method, now receiveing json response.

--- app/src/main/java/comnathanromike/httpsgithub/locateeatrepeat/RestaurantsActivity.java
+++ app/src/main/java/comnathanromike/httpsgithub/locateeatrepeat/RestaurantsActivity.java
@@ -3,30 +3,63 @@ package comnathanromike.httpsgithub.locateeatrepeat;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class RestaurantsActivity extends AppCompatActivity {
-    private ListView mListView;
-    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro", "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",  "Slappy Cakes", "Equinox", "Miss Delta's", "Andina", "Lardo", "Portland City Grill", "Fat Head's Brewery", "Chipotle", "Subway"};
+import java.io.IOException;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class RestaurantsActivity extends AppCompatActivity {
     public static final String TAG = RestaurantsActivity.class.getSimpleName();
-    private TextView mLocationTextView;
+    @Bind(R.id.listView) ListView mListView;
+    @Bind(R.id.locationTextView) TextView mLocationTextView;
+
+    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro", "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",  "Slappy Cakes", "Equinox", "Miss Delta's", "Andina", "Lardo", "Portland City Grill", "Fat Head's Brewery", "Chipotle", "Subway"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
-
-        mListView = (ListView) findViewById(R.id.listView);
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
-        mListView.setAdapter(adapter);
-
-        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
+
+        getRestaurants(location);
+
+//        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
+//        mListView.setAdapter(adapter);
+
         mLocationTextView.setText("Here are all the restaurants near: " + location);
+    }
+
+    private void getRestaurants(String location) {
+        final YelpService yelpService = new YelpService(this);
+
+        yelpService.findRestaurants(location, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String jsonData = response.body().string();
+                    if (response.isSuccessful()) {
+                        Log.v("JSON DATA", jsonData);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
